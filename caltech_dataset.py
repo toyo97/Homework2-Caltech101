7971ecb99650ca264ed2857b3070b1ba5711903f
Vittorio <zampinetti@gmail.com>
2020-04-30 12:27:40 +0200

    Implemented class constructor, getter and len methods: all images in main memory approach.

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -16,12 +16,22 @@ def pil_loader(path):
 
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
+        """
+        Contains the images defined in the split file along with their class label. To match the class index
+        to the actual label name, find the index in the label_names attribute of the object.
+
+        :param root: path to root directory
+        :param split: name of the txt file containing all the relative paths of the images in the split
+        :param transform: torchvision transforms object, transformation applied on the image during the access
+        :param target_transform: torchvision transforms object, transformation applied on the target
+        """
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
+        self.split = split  # This defines the split you are going to use
+        # (split files are called 'train.txt' and 'test.txt')
 
         '''
+        INSTRUCTIONS
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
         - PyTorch Dataset classes use indexes to read elements
@@ -30,19 +40,40 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
-    def __getitem__(self, index):
-        '''
+        # All data in memory approach
+        self.label_names = {}
+
+        self.imgs = []
+        self.labels = []
+
+        class_counter = 0
+        for line in open(split + '.txt', 'r'):
+            if line.find('BACKGROUND') == -1:
+                img = pil_loader(os.path.join(data_dir, line.strip()))
+                self.imgs.append(img)
+
+                # Extract label name and, if not already seen, associate an incremental id
+                label_name = line.split('/')[0]
+
+                if label_name not in self.label_names:
+                    self.label_names[label_name] = class_counter
+                    class_counter += 1
+
+                self.labels.append(self.label_names[label_name])
+
+        assert class_counter == 101
+
+    def __getitem__(self, index) -> (Image, int):
+        """
         __getitem__ should access an element through its index
-        Args:
-            index (int): Index
 
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        '''
+            :param index: int, direct access index
+            :return: tuple: (sample, target) where target is class_index of the target class.
+        """
 
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+        image, label = self.imgs[index], self.labels[index]  # Provide a way to access image and label via index
+        # Image should be a PIL Image
+        # label can be int
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
@@ -51,9 +82,21 @@ class Caltech(VisionDataset):
         return image, label
 
     def __len__(self):
-        '''
+        """
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
-        '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+
+        :return: int
+        """
+        length = len(self.imgs)  # Provide a way to get the length (number of elements) of the dataset
         return length
+
+
+# Testing the methods
+if __name__ == '__main__':
+
+    data_dir = '101_ObjectCategories'
+    cal = Caltech(data_dir, split='test')
+    image, label = cal[0]
+    image.show()
+    print(len(cal))
